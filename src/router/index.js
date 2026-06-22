994f33b29208f03f0697261fbb8f84f8aba3d831
arjaycuanan <cuananarjay.ca@gmail.com>
2026-06-22 20:22:37 +0800

    Changes made to route path of edit button

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
         meta: { requiresAuth: true }
     },
     {
-        path: '/edit/:id',
+        path: '/posts/edit/:id',
         name: 'edit-post',
         component: EditPostView,
         meta: { requiresAuth: true }
